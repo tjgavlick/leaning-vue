344303b9ed50153b5dd72df0a90ac731dc13c4cd
tjgavlick <tjgavlick@gmail.com>
2018-04-16 08:49:38 -0700

    blank out sandbox

--- sandbox/app.js
+++ sandbox/app.js
@@ -4,15 +4,8 @@ var vue = new Vue({
   el: '#app',
 
   data: {
-    title: 'Hello, world',
-    link: 'https://google.com',
-    anchor: '<a href="https://google.com">The Googs</a>'
   },
 
   methods: {
-    sayHello: function () {
-      this.title = "Goodbye";
-      return this.title;
-    }
   }
 });
