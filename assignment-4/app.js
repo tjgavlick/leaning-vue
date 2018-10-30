5632ba5dbb35d7da98bccd082971c5098e0117e9
tjgavlick <8879532+tjgavlick@users.noreply.github.com>
2018-10-30 15:06:53 -0700

    Complete assignment 4

--- assignment-4/app.js
+++ assignment-4/app.js
@@ -1,11 +1,35 @@
 new Vue({
   el: '#exercise',
-  data: {
 
+  data: {
+    class1: 'highlight',
+    class2: 'foo',
+    class3: 'bar',
+    userClasses: '',
+    hasBorder: 'false',
+    progress: 0
   },
+
+  computed: {
+    boolHasBorder: function () {
+      return this.hasBorder === 'true';
+    }
+  },
+
   methods: {
     startEffect: function() {
-    
+      setInterval(function () {
+        this.class1 = this.class1 === 'highlight' ? 'shrink' : 'highlight';
+      }.bind(this), 500);
+    },
+
+    startProgress: function () {
+      var interval = setInterval(function () {
+        this.progress++;
+        if (this.progress >= 100) {
+          clearInterval(interval);
+        }
+      }.bind(this), 16.67);
     }
   }
 });
