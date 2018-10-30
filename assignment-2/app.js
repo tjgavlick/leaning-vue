96cf2a279e69511cda37e11ad7274ac53e66624a
tjgavlick <8879532+tjgavlick@users.noreply.github.com>
2018-10-30 13:50:50 -0700

    Complete assignment 2

--- assignment-2/app.js
+++ assignment-2/app.js
@@ -1,6 +1,16 @@
 new Vue({
   el: '#exercise',
+
   data: {
     value: ''
+  },
+
+  methods: {
+    showAlert: function () {
+      alert("aaaaa");
+    },
+    updateValue: function (ev) {
+      this.value = ev.target.value;
+    }
   }
 });
