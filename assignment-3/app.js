9f486a119e29b695ceba9508fbb64ff793a01bea
tjgavlick <8879532+tjgavlick@users.noreply.github.com>
2018-10-30 14:18:46 -0700

    Complete assignment 3

--- assignment-3/app.js
+++ assignment-3/app.js
@@ -1,6 +1,21 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            value: 0
-        }
-    });
+  el: '#exercise',
+
+  data: {
+    value: 0
+  },
+
+  computed: {
+    result: function () {
+      return this.value === 37 ? "done" : "not there yet";
+    }
+  },
+
+  watch: {
+    result: function () {
+      setTimeout(function () {
+        this.value = 0;
+      }.bind(this), 5000);
+    }
+  }
+});
