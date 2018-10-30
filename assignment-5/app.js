aa27fe7911d1a7a39cc2a168e4c924d19affee0b
tjgavlick <8879532+tjgavlick@users.noreply.github.com>
2018-10-30 15:52:59 -0700

    Complete assignment 5

--- assignment-5/app.js
+++ assignment-5/app.js
@@ -1,6 +1,8 @@
 new Vue({
   el: '#exercise',
+
   data: {
+    showParagraphs: true,
     array: ['Max', 'Anna', 'Chris', 'Manu'],
     myObject: {
       title: 'Lord of the Rings',
@@ -8,7 +10,7 @@ new Vue({
       books: '3'
     },
     testData: {
-      name: 'TESTOBJECT', 
+      name: 'TESTOBJECT',
       id: 10,
       data: [1.67, 1.33, 0.98, 2.21]
     }
