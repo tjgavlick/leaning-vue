a5cd07a99ab0cc1fb2e8be98ff728c4356e0bca5
tjgavlick <8879532+tjgavlick@users.noreply.github.com>
2018-11-02 15:24:03 -0700

    Add sandbox area via vue-cli 3

---
+++ sandbox/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Hello, World" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
